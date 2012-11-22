9fb712093c3a5c043acd310de3a11a1b23c9a8a9
Eduardo Gondim <egondim@gmail.com>
2012-11-22 09:33:03 -0200

    v4
    refeito o calculo do x e y

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@
 module Main where
 
 import Text.Printf -- Oba, Haskell tem printf! :-)
+import System.Random
 
 type Point     = (Float,Float)
 type Color     = (Int,Int,Int)
@@ -43,35 +44,52 @@ svgCloudGen w h dataset =
         "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
         "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" ++
         (svgViewBox w h) ++
-        (concat (gera w h 0 dataset)) ++ "</svg>\n"
+        (concat (gera 175.0 175.0 50 dataset)) ++ "</svg>\n"
 
 
 -- Esta funcao deve gerar a lista de circulos em formato SVG.
 -- A implementacao atual eh apenas um teste que gera um circulo posicionado no meio da figura.
 -- TODO: Alterar essa funcao para usar os dados do dataset.
+{-
+espiral :: [Int] -> [Int]
+espiral d = let
+	ang = getStdRandom (randomR (0, 360))
+	antx = 175
+	anty = 175
+	in [ x | x <- (ang/pi)*180
+-}
 
-gera :: Int -> Int -> Int -> [Int] -> [String]
+--rN :: IO a -> Int 
+--rN a = getStdRandom (randomR (0, a :: Int))
+
+gera :: Float -> Float -> Int -> [Int] -> [String]
 gera w h ad [] = []
-gera w h ad (d:s) = svgBubbleGen w h d ad : gera w h (head s) s
+gera w h ad (d:s) = let
+	x = spiralw w ad
+	y = spiralh h ad
+	r = fromIntegral d/10+5
+	ad = ad+10
+	in svgBubbleGen x y r : gera (w + x) (h + y) ad s
 	
-spiralw :: Int -> Int -> Float
-spiralw x r = fromIntegral r/2
+--gera w h ad (d:s) =	svgBubbleGen w h d ad : gera w h (ad + 10) s
+	
+	
+spiralw :: Float -> Int -> Float
+spiralw x r = abs(x/2 * cos (fromIntegral 360/pi * 180))
 
-spiralh :: Int -> Int -> Float
-spiralh y r = fromIntegral r/2
+spiralh :: Float -> Int -> Float
+spiralh y r = abs(y/2 * sin (fromIntegral 360/pi * 180))
 
-svgBubbleGen:: Int -> Int -> Int -> Int -> String
-svgBubbleGen w h d ad 
-	| d >= 82 = svgCircle (( (spiralw w ad), (spiralh h ad)), fromIntegral 90)
-	| otherwise = svgCircle (( (spiralw w ad), (spiralh h ad)), fromIntegral d/10+5)
-
+svgBubbleGen:: Float -> Float -> Float -> String
+svgBubbleGen w h r = svgCircle ((w, h), r)
+	
 -- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
 -- TODO: Alterar esta funcao para mostrar um circulo de uma cor fornecida como parametro.
 svgCircle :: Circle -> String
 svgCircle ((x,y),r)	
 	| r <= 5.5 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(44,122,245)\" />\n" x y r
 	| r > 5.5 && r <= 6 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(12,222,45)\" />\n" x y r
-	| r > 6 && r <= 11.5 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(222,156,45)\" />\n" x y r
+	| r > 6 && r <= 11.5 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(22,156,45)\" />\n" x y r
 	| r > 11.5 && r <= 17 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(0,252,177)\" />\n" x y r
 	| r > 17 && r <= 22 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(2,22,45)\" />\n" x y r
 	| r > 22 && r <= 27 = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(15,202,55)\" />\n" x y r
